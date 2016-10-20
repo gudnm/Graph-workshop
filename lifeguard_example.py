5bbf1172c4d2634978428ecf33e60b2ce76cbb40
Ivan Vashchenko <srochno@gmail.com>
2016-10-20 11:10:53 -0400

    Update lifeguard_example.py
    Add deque (double-ended queue) to replace list as a data structure used for queue functionality

--- lifeguard_example.py
+++ lifeguard_example.py
@@ -1,17 +1,20 @@
+from collections import deque
+
 def get_neighbors( pool_coords, pool ):
     x, y = pool_coords
     max_x, max_y = len( pool ), len( pool[ 0 ] )
     potential_neighbors = [ ( x + 1, y ), ( x - 1, y ), ( x, y + 1 ), ( x, y - 1 ) ]
     pool_bounds_filter = lambda z: 0 <= z[ 0 ] < max_x and 0 <= z[ 1 ] < max_y
     rope_filter = lambda z: pool[ z[ 0 ] ][ z[ 1 ] ] != "X"
-    return filter( rope_filter, filter( pool_bounds_filter, potential_neighbors ) )
+    return list( filter( rope_filter, filter( pool_bounds_filter, potential_neighbors ) ) )
 
 def lifeguard_within_k_steps( pool, starting_coords, k ):
-    queue = [ ( 0, starting_coords ) ] # set up data structures
+    queue = deque() # set up data structures
+    queue.append( ( 0, starting_coords ) )
     visited = set()
 
     while len( queue ) != 0:
-        current_distance, current_location = queue.pop( 0 ) # inefficient, should use deque
+        current_distance, current_location = queue.popleft()
         if current_distance > k: return False # if we've gone more than k steps, fail
         if pool[ current_location[ 0 ] ][ current_location[ 1 ] ] == "L": return True
 
